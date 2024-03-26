f778f806be5c32428e821e2410d001afc5f013b0
wittlsn <simon.wittl@th-deg.de>
2024-03-26 08:45:29 +0100

    added experiment 00

---
+++ experiments/experiment_00_simple/generate_data_00.py
@@ -0,0 +1,50 @@
+from artistlib import API, SAVEMODES
+from artistlib.trajectory import sphere_trajectory
+from pathlib import Path
+import json
+import numpy as np
+
+
+SCENE_PATH = Path.cwd() / 'experiments' / 'experiment_00_simple' / 'scene_experiment_00.aRTist'
+MESEHES_DICT_PATH = Path.cwd() / 'meshes' / 'data_dict.json'
+FOD_MM = 1000.
+FDD_MM = 2000.
+NUMBER_OF_PROJECTIONS = 500
+SAVE_FOLDER = Path.cwd() / 'experiments' / 'experiment_00_simple' / 'data'
+MIN_Z_POSITION_MM = -70
+MAX_Z_POSITION_MM = 60
+
+
+
+def main():
+    # Make save folder, clear scene and load scene
+    SAVE_FOLDER.mkdir(exist_ok=True)
+    artist = API()
+    artist.clear_scene()
+    artist.load_project(SCENE_PATH)
+    
+    # Load meshes into scene
+    with open(str(MESEHES_DICT_PATH), 'r') as f:
+        data_dict = json.load(f)
+
+    artist.load_part(Path(data_dict['hull']['path']), 'PE (HDPE)')
+    for hexagon_dict in data_dict['hexagon_poses']:
+        position = np.array(hexagon_dict['matrix'])[:3, 3]
+        z_position = position[2]
+        if MIN_Z_POSITION_MM > z_position or  z_position > MAX_Z_POSITION_MM:
+            artist.load_part(Path(hexagon_dict['part']), 'W')
+
+    # generate
+    source_positions, detector_positions, orientation = sphere_trajectory(FOD_MM, FDD_MM, NUMBER_OF_PROJECTIONS)
+    for i in range(NUMBER_OF_PROJECTIONS):
+        artist.translate('S', *source_positions[i])
+        artist.translate('D', *detector_positions[i])
+        artist.rotate_from_rotation_matrix('S', orientation[i])
+        artist.rotate_from_rotation_matrix('D', orientation[i])
+        artist.save_image(SAVE_FOLDER / f'projection_{i:04}.tif', 
+                          save_mode=SAVEMODES.UINT16, 
+                          save_projection_geometry=True)
+
+
+if __name__ == '__main__':
+    main()
--- generate_hexagon_sphere.py
+++ generate_hexagon_sphere.py
@@ -2,26 +2,34 @@ import trimesh
 import numpy as np
 from scipy.spatial.transform import Rotation
 from pathlib import Path
+import json
+
+
+
+OUTER_RADIUS = 230
+INNER_RADIUS = 220
+HEXAGON_PARTS = 55 # number of Hexagon parts
+HEXAGON_TOP_THICKNESS = 10 # top additional. The overall hexagon thickness is outer_r - innner_r + thickness
+SIDE_LENGTH_FAKTOR = 0.99 # Fit in sphere holes
+TOP_LENGTH_FACTOR = 1.25 # extended top hexagons
+BOTTOM_HEIGHT = 40 # Heigth of the bottom sample holder
+SAVE_FOLDER = Path('./meshes')
+DATAFORMAT = '.obj'
 
 
 def calculate_theta_k(theta_k_prior: float, h_k: float, N: float) -> float:
     return (theta_k_prior + 3.6 / np.sqrt(N) * 1. / np.sqrt(1. - h_k ** 2)) % (2 * np.pi)
+
 
 def calculate_single_vector(theta, sigma):
         vector = np.array([0, 0, 1]).reshape((3, 1))
         vector = Rotation.from_euler('z', theta).as_matrix() @ Rotation.from_euler('x', sigma).as_matrix() @ vector
         return vector
 
-OUTER_RADIUS = 250
-INNER_RADIUS = 240
-HEXAGON_PARTS = 50 # number of Hexagon parts
-HEXAGON_TOP_THICKNESS = 10 # top additional. The overall hexagon thickness is outer_r - innner_r + thickness
-SIDE_LENGTH_FAKTOR = 0.99 # Fit in sphere holes
-TOP_LENGTH_FACTOR = 1.25 # extended top hexagons
-SAVE_FOLDER = Path('./meshes')
-
 
 def main():
+    data_dict = dict()
+
     SAVE_FOLDER.mkdir(exist_ok=True)
     hexagon_heigth = OUTER_RADIUS - INNER_RADIUS  
     outer_sphere = trimesh.creation.uv_sphere(OUTER_RADIUS, (180, 180))
@@ -57,7 +65,8 @@ def main():
 
 
     hexagon = trimesh.boolean.union([hexagon, hexagon_add])
-    hexagon.export(SAVE_FOLDER / 'hexagon.stl')
+    hexagon_path = SAVE_FOLDER / f'hexagon{DATAFORMAT}'
+    hexagon.export(hexagon_path)
 
     side_length = distance_mm * SIDE_LENGTH_FAKTOR * 1.05
     angle_offset = np.pi / 6
@@ -69,7 +78,9 @@ def main():
 
     hull = trimesh.boolean.difference([outer_sphere, inner_sphere])
 
-    for i in range(HEXAGON_PARTS // 10, HEXAGON_PARTS - 1):
+    hexagon_trafo = list()
+
+    for i in range(HEXAGON_PARTS // 10, HEXAGON_PARTS-(HEXAGON_PARTS // 10)):
 
         radius = INNER_RADIUS - (OUTER_RADIUS - INNER_RADIUS) / 2 - hexagon_heigth * 2
         rotation_matrix = Rotation.from_euler('ZX', np.array([theta_k[i], sigma_k[i]])).as_matrix()
@@ -90,14 +101,68 @@ def main():
         rotation_matrix_eye_y[:3, :3] = rotation_matrix_y
         translation = np.matmul(rotation_matrix, np.array([0, 0, radius + (OUTER_RADIUS - INNER_RADIUS) *2 + HEXAGON_TOP_THICKNESS * 2]))
         translation = trimesh.transformations.translation_matrix(translation)
-        hexagon_save_part.apply_transform(rotation_matrix_eye_y)
-        hexagon_save_part.apply_transform(rotation_matrix_eye)
-        hexagon_save_part.apply_transform(translation)
-        hexagon_save_part.export(SAVE_FOLDER / f'part_{i}.stl')
 
-    hull.export(SAVE_FOLDER / 'hull.stl')
+        hexagon_transformation = rotation_matrix_eye_y.copy()
+        hexagon_transformation = np.matmul(rotation_matrix_eye, hexagon_transformation)
+        hexagon_transformation: np.ndarray = np.matmul(translation, hexagon_transformation)
+        hexagon_save_part.apply_transform(hexagon_transformation)
 
-    # TODO: add platform for mounting
+        hexagon_save_part_path = SAVE_FOLDER / f'part_{i}{DATAFORMAT}'
+        hexagon_save_part.export(hexagon_save_part_path)
+
+        hexagon_trafo.append({
+            'matrix': hexagon_transformation.tolist(),
+            'untransformed': str(hexagon_path),
+            'part': str(hexagon_save_part_path)
+        })
+
+    data_dict['hexagon_poses'] = hexagon_trafo
+
+
+    cut_out_top = trimesh.creation.cylinder(OUTER_RADIUS * 1 / 3, OUTER_RADIUS * 2)
+    cut_out_top.apply_translation(np.array([0, 0, -OUTER_RADIUS]))
+    hull = trimesh.boolean.difference([hull, cut_out_top])
+
+    bottom = trimesh.creation.cylinder(OUTER_RADIUS * 1 / 3, BOTTOM_HEIGHT)
+    bottom.apply_translation(np.array([0, 0, OUTER_RADIUS - (BOTTOM_HEIGHT * 0.5)]))
+
+
+    bottom_hexagon: trimesh.Trimesh = hexagon.copy()
+    
+    
+    bottom_transformation = trimesh.transformations.scale_and_translate(1, np.array([0, 0, OUTER_RADIUS - BOTTOM_HEIGHT/2 - HEXAGON_TOP_THICKNESS*2]))
+    bottom_hexagon.apply_transform(bottom_transformation)
+
+    bottom = trimesh.boolean.difference([bottom, bottom_hexagon])
+    bottom_hexagon.apply_scale(SIDE_LENGTH_FAKTOR)
+
+    hull = trimesh.boolean.union([hull, bottom])
+
+    screws1 = trimesh.creation.cylinder(6, OUTER_RADIUS * 3)
+    screws2 = screws1.copy()
+
+    screws1.apply_translation([25, 0, 0])
+    screws2.apply_translation([-25, 0, 0])
+
+    hull = trimesh.boolean.difference([hull, screws1])
+    hull = trimesh.boolean.difference([hull, screws2])
+    bottom_hexagon = trimesh.boolean.difference([bottom_hexagon, screws1])
+    bottom_hexagon = trimesh.boolean.difference([bottom_hexagon, screws2])
+
+    bottom_hexagon_path = SAVE_FOLDER / f'sample_holder_mount{DATAFORMAT}'
+    bottom_hexagon.export(bottom_hexagon_path)
+
+    hull_path = SAVE_FOLDER / f'hull{DATAFORMAT}'
+    hull.export(hull_path)
+
+    data_dict['hull'] = {'path': str(hull_path),
+                         'matrix': np.eye(4).tolist()}
+    data_dict['sample_holder'] = {'path': str(bottom_hexagon_path),
+                                  'matrix': np.eye(4).tolist()}
+
+
+    with open(str(SAVE_FOLDER / 'data_dict.json'), 'w') as f:
+        json.dump(data_dict, f, indent=4)
 
 if __name__ == '__main__':
     main()
